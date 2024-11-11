a5f8f02b0fde9ae601da28906d34bd706513afbe
Daniel Levy <daniel.levy04@post.runi.ac.il>
2024-11-11 13:07:41 +0200

    first commit

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int totalBill = Integer.parseInt (args[3]);
+		double price = totalBill;
+		double eachPrice = Math.ceil(price / 3);
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + 
+							": pay " + eachPrice + " Shekels each.");
 	}
 }
